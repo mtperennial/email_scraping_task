acf625338e54c244fbe54ccbb06391aaf2a5f025
mohdtausif <mohd.tausif@perennialsys.com>
2022-04-27 18:22:02 +0530

    create a general function for scrap the email and move it to tha helper.js file

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,3 +1,6 @@
+const appRoot = require('app-root-path');
+const config = require(appRoot + '/config.json');
+
 async function sleep(time = 3000) {
     return timeout(time);
 }
@@ -29,9 +32,70 @@ async function waitForFunction(testFunction, timeoutMs) {
     });
 };
 
+function getEmailContent(email) {
+    return `
+        console.warn("========= working inside helper function ===========")
+        var response = JSON.parse(sessionStorage.getItem("response"));
+        var emailData = {};
+        var div = document.getElementsByClassName("gmail_default")[0];
+
+        emailData.id = Date.now();
+        if(div == null || div == undefined) {
+            var tbl = document.querySelectorAll("table")[10].querySelector("tr");
+            emailData.message = tbl.innerText
+        }
+        else{
+            emailData.message = div.textContent;
+        }
+        emailData.attachmentsCount = 0;
+        emailData.attachments = [];
+
+        var attachmentsDiv = document.getElementsByClassName("aSH");
+        var file = document.getElementsByClassName("N5jrZb")
+        console.warn('attachmentsDiv:===>>>>', attachmentsDiv)
+
+        if(attachmentsDiv.length > 0) {
+            var attachmentsArr = []
+            for (var i = 0; i < attachmentsDiv.length; i++) {
+                var fileObj = {};
+                var attachment = attachmentsDiv[i].querySelector("span")
+                // file[i].classList.add("aZp")
+                fileObj.name = attachment.textContent;
+                fileObj.url = '${config.emailsDownloadPath}'+'${email}/'+attachment.textContent;
+                // console.warn('fileObj:', fileObj)
+                emailData.attachments.push(fileObj);
+                attachmentsArr.push(file[i].querySelector("a"));
+            }
+
+            if (attachmentsArr.length > 0) {
+                var interval = setInterval(download, 1000, attachmentsArr);
+                function download(attachmentsArray) {
+                    console.warn('attachmentsArray:11111111=======>>>>>>', attachmentsArray)
+                    var url = attachmentsArray.pop();
+                    var a = document.createElement('a');
+                    a.setAttribute('href', 'url');
+                    a.setAttribute('download', '');
+                    a.setAttribute('target', '_blank');
+                    a.append(url);
+                    a.querySelector('a').click();
+                    if (attachmentsArray.length == 0) {
+                        clearInterval(interval);
+                    }
+                    a.removeChild(url);
+                    a.remove();
+                }
+            }
+        }
+        emailData.attachmentsCount = emailData.attachments.length;  // ! to check how many attachments are there
+        response.push(emailData);
+        sessionStorage.setItem("response", JSON.stringify(response));
+    `
+}
+
 module.exports = {
     sleep,
     timeout,
     waitForFunction,
+    getEmailContent,
 };
 
